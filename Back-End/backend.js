9b516d6be630c109367d994344d35b8532f1972f
Vincent GUILLON <vincent.guillon@hotmail.fr>
2019-02-12 15:31:22 -0500

    npm install updated

--- Back-End/backend.js
+++ Back-End/backend.js
@@ -18,7 +18,6 @@ const fs = require('fs');
 const Hapi = require('hapi');
 const path = require('path');
 const Boom = require('boom');
-const color = require('color');
 const ext = require('commander');
 const jsonwebtoken = require('jsonwebtoken');
 const request = require('request');
