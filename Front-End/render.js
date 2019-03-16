3de2111549c4dc2b38890638709b6bfed2e2001f
Vincent GUILLON <vincent.guillon@hotmail.fr>
2019-03-16 17:07:22 -0400

    Skybox added + bug fix + mobile WIP

--- Front-End/render.js
+++ Front-End/render.js
@@ -50,6 +50,16 @@ var createScene = function () {
 	// Set the Background color (RGBA)
 	scene.clearColor = new BABYLON.Color4(0,0,0,0);
 
+	//Setup of the skybox
+	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+	var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+	skyboxMaterial.backFaceCulling = false;
+	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/skybox/skybox", scene);
+	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+	skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+	skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+	skybox.material = skyboxMaterial;
+
 	return scene;
 };
 
@@ -62,7 +72,7 @@ scene.registerBeforeRender(function() {   scene.activeCamera.alpha += 0.0005;});
 scene.onPointerPick = function (evt, pickInfo) {
 	
 	// Check if the mesh is selectable
-	if(!pickInfo.pickedMesh.name.startsWith("NoZone"))
+	if(!pickInfo.pickedMesh.name.startsWith("NoZone") && !pickInfo.pickedMesh.name.startsWith("skyBox"))
 	{
 		//Check if a mesh as already been selected
 		if(actuallySelected)
